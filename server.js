0ebe0ff1e9080b5e8f7bc6e107c8098d1e8c932d
Peerapat.K <peerapat.work.formal@gmail.com>
2026-08-30 18:27:00 +0700

    Chapter5&6

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ success:true, data:{id:1} });
-});
+const hospitals = require('./routres/hospitals');
+app.use('/api/v1/hospitals', hospitals);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT));
